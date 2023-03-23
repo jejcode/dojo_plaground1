3502713341e3b1fef3fd2aac21e31b7ee51d7db1
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-03-23 16:02:22 -0600

    fixed functions to show all assgn levels

--- files/server.py
+++ files/server.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template # render_template necessary for templates to work
 app = Flask(__name__) # new instance of Flask
 
+@app.route('/play')  # route that will load play.html
+def play(num = 3, color = "rgb(144,192,243)"):
+    #     ^ function arguments are passed from route line   v
+    return render_template('play.html', num = num, color = color) 
+
+@app.route('/play/<int:num>')  # route that will load play.html
+def play_num(num, color = "rgb(144,192,243)"):
+    #     ^ function arguments are passed from route line   v
+    return render_template('play.html', num = num, color=color) 
+
 @app.route('/play/<int:num>/<string:color>')  # route that will load play.html
-def play(num, color):
+def play_num_color(num, color):
     #     ^ function arguments are passed from route line   v
     return render_template('play.html', num = num, color = color) 
 
